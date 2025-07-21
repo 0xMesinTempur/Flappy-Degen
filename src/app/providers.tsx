1bf776aa6fe422f2215152519bd2027eb911e6f7
0xMesinTempur <aditnugraha445@gmail.com>
2025-07-21 09:22:30 +0700

    fix: correct wagmiConfig import to wagmiconfig

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { WagmiProvider } from 'wagmi';
-import { config } from '../wagmiConfig';
+import { config } from '../wagmiconfig';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 
